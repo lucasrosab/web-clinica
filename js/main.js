9e871286310441f831875205ad4f314fa65aaf24
Lucas Rosa Barbosa <lucas.rosa2006@gmail.com>
2019-03-19 10:52:44 -0300

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// Página de ajustes e verificações JavaScript 
+
+
+//Inicialização do SideNav do Materializer
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+  });
---
+++ js/src/module.main.js
@@ -0,0 +1,6 @@
+/**
+ * Esta página, cria o módulo central da aplicação, atraves desse módulo
+ * todo o escopo da aplicação é definido.
+ */
+
+var app = angular.module('app-angular', []);
