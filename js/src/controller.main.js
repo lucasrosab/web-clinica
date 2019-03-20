6f0e0b429e095e2fb3c596745813df760753bb8e
Lucas Rosa Barbosa <lucas.rosa2006@gmail.com>
2019-03-19 21:57:43 -0300

    Initial Commit

---
+++ js/src/controller.main.js
@@ -0,0 +1,42 @@
+app.controller('Main', function($scope, $http){
+
+    $scope.agenda = [
+        {
+            id: 0,
+            paciente: 'Lucas',
+            inicio: '12:00',
+            psicologo: 'Gutemberg'
+        },
+        {
+            id: 1,
+            paciente: 'Lucas',
+            inicio: '12:00',
+            psicologo: 'Gutemberg'
+        },
+        {
+            id: 2,
+            paciente: 'Lucas',
+            inicio: '12:00',
+            psicologo: 'Gutemberg'
+        },
+    ];
+
+
+    
+    function carregarAgenda(){
+        $http({
+            method : "GET",
+            url : "welcome.htm"
+          }).then(function mySuccess(response) {
+            $scope.agenda = response.data;
+          }, function myError(response) {
+                        
+            $scope.agenda = response.statusText;
+          });
+    }
+
+    
+
+
+
+});
